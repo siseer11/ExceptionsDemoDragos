108ef111c437773da0230cc8c7f46ab13e410bb1
Jonatan Streith <jonatan@streith.se>
2026-05-25 12:00:32 +0200

    Stuff

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@
             {
                 Console.WriteLine("=== Start av programmet ===");
 
+                //Stuff
+
+
                 // Exempel 1: try-catch-finally
                 try
                 {
